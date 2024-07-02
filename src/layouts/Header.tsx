53cd31dde3e97fa6747946f18a28880f68dfe82a
Aidin <aidinjavascript@gmail.com>
2024-07-02 15:48:08 +0600

    header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,10 +1,21 @@
+import { Box, Typography } from "@mui/material";
 
 const Header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	return (
+		<div>
+			<Box
+				sx={{
+					width: "100%",
+					height: "100px",
+					backgroundColor: "black",
+				}}>
+				<Typography
+					sx={{ textAlign: "center", fontSize: "50px", color: "white" }}>
+					Проект
+				</Typography>
+			</Box>
+		</div>
+	);
+};
 
-export default Header
+export default Header;
